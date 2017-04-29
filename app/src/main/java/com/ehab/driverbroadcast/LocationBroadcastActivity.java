cc3b7c03afe2154774d896bcdae35e920580d1d6
ehabhamdy <ehabhamdy2012@gmail.com>
2017-04-29 18:45:31 +0200

    Initial commit

---
+++ app/src/main/java/com/ehab/driverbroadcast/LocationBroadcastActivity.java
@@ -0,0 +1,197 @@
+package com.ehab.driverbroadcast;
+
+import android.content.IntentSender;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResult;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
+import com.pubnub.api.PNConfiguration;
+import com.pubnub.api.PubNub;
+import com.pubnub.api.callbacks.PNCallback;
+import com.pubnub.api.models.consumer.PNPublishResult;
+import com.pubnub.api.models.consumer.PNStatus;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class LocationBroadcastActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, LocationListener {
+
+    private GoogleApiClient mGoogleClientApi;
+    private LocationRequest mLocationRequest;
+    PendingResult<LocationSettingsResult> result;
+
+    private PubNub mPubnub;
+
+    public static final String SUB_KEY = BuildConfig.SUB_KEY;
+    public static final String PUB_KEY = BuildConfig.PUB_KEY;
+
+    private static final String[] LOCATION_PERMS = {
+            android.Manifest.permission.ACCESS_FINE_LOCATION
+    };
+    private static final int LOCATION_REQUEST = 50;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_location_broadcast);
+
+        this.buildGoogleApiClient();
+
+        PNConfiguration pnConfiguration = new PNConfiguration();
+        pnConfiguration.setSubscribeKey(SUB_KEY);
+        pnConfiguration.setPublishKey(PUB_KEY);
+
+        mPubnub = new PubNub(pnConfiguration);
+
+        Button finishBtn = (Button) findViewById(R.id.finishBtn);
+        finishBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mGoogleClientApi.connect();
+    }
+
+    @Override
+    protected void onStop() {
+        mGoogleClientApi.disconnect();
+        super.onStop();
+    }
+
+
+    private synchronized void buildGoogleApiClient() {
+        mGoogleClientApi = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addApi(LocationServices.API)
+                .build();
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        LocationRequest mLocationRequest = createLocationRequest();
+
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, android.Manifest.permission.ACCESS_FINE_LOCATION)) {
+
+                ActivityCompat.requestPermissions(this,
+                        LOCATION_PERMS,
+                        LOCATION_REQUEST);
+            } else {
+                ActivityCompat.requestPermissions(this,
+                        LOCATION_PERMS,
+                        LOCATION_REQUEST);
+            }
+            return;
+        }
+
+        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleClientApi, mLocationRequest, this);
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    private LocationRequest createLocationRequest() {
+        LocationRequest mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(10000);
+        mLocationRequest.setFastestInterval(5000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        return mLocationRequest;
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        Log.d("Location Update", "Latitude: " + location.getLatitude() +
+                " Longitude: " + location.getLongitude());
+        broadcastLocation(location);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(ActivityCompat.checkSelfPermission(this, permissions[0]) == PackageManager.PERMISSION_GRANTED) {
+            if(requestCode == LOCATION_REQUEST){
+                askForGPS();
+            }
+        }
+
+    }
+
+    private void broadcastLocation(Location location) {
+        JSONObject message = new JSONObject();
+        try {
+            message.put("lat", location.getLatitude());
+            message.put("lng", location.getLongitude());
+            message.put("alt", location.getAltitude());
+        } catch (JSONException e) {
+            //Log.e(TAG, e.toString());
+        }
+        Toast.makeText(this, "Sent", Toast.LENGTH_SHORT).show();
+        mPubnub.publish()
+                .message(message)
+                .channel("awesome-channel")
+                .async(new PNCallback<PNPublishResult>() {
+                    @Override
+                    public void onResponse(PNPublishResult result, PNStatus status) {
+                        // handle publish result, status always present, result if successful
+                        // status.isError to see if error happened
+                        if(status.isError()){
+                            Toast.makeText(LocationBroadcastActivity.this, "Error", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    private void askForGPS() {
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder().addLocationRequest(mLocationRequest);
+        builder.setAlwaysShow(true);
+        result = LocationServices.SettingsApi.checkLocationSettings(mGoogleClientApi, builder.build());
+        result.setResultCallback(new ResultCallback<LocationSettingsResult>() {
+            @Override
+            public void onResult(LocationSettingsResult result) {
+                final Status status = result.getStatus();
+                switch (status.getStatusCode()) {
+                    case LocationSettingsStatusCodes.SUCCESS:
+                        break;
+                    case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+                        try {
+                            status.startResolutionForResult(LocationBroadcastActivity.this, 100);
+                        } catch (IntentSender.SendIntentException e) {
+
+                        }
+                        break;
+                    case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
+
+                        break;
+                }
+            }
+        });
+    }
+}
