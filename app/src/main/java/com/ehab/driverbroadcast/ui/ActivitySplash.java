5a459421ac1fbfa455ff562da4c90130cb05855a
ehabhamdy <ehabhamdy2012@gmail.com>
2017-07-03 19:41:19 +0200

    Enhancing splash screen consistency

--- app/src/main/java/com/ehab/driverbroadcast/ui/ActivitySplash.java
+++ app/src/main/java/com/ehab/driverbroadcast/ui/ActivitySplash.java
@@ -3,6 +3,7 @@ package com.ehab.driverbroadcast.ui;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -11,58 +12,41 @@ import com.ehab.driverbroadcast.R;
 
 public class ActivitySplash extends ActivityBase {
 
-    private static int SPLASH_TIME_OUT = 1000;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
 
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
 
-        new Handler().postDelayed(new Runnable() {
+        if (user != null) {
 
-            /*
-             * Showing splash screen with a timer. This will be useful when you
-             * want to show case your app logo / company
-             */
+            if (user.isEmailVerified()) {
+                Intent intent = new Intent(ActivitySplash.this, LocationBroadcastActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                startActivity(intent);
+                overridePendingTransition(0,0);
+            } else {
+                // User signed out or No Network Connection
+                Intent intent = new Intent(ActivitySplash.this, ActivityLogin.class);
 
-            @Override
-            public void run() {
-
-
-                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-
-                if (user != null) {
-
-                    if (user.isEmailVerified()) {
-                        Intent intent = new Intent(ActivitySplash.this, LocationBroadcastActivity.class);
-                        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                        startActivity(intent);
-
-                    } else {
-                        // User signed out or No Network Connection
-                        Intent intent = new Intent(ActivitySplash.this, ActivityLogin.class);
-
-                        //Removing HomeActivity from the back stack
-                        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                        startActivity(intent);
-                        //overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
-                    }
-
-
-                }else{
-                    Intent intent = new Intent(ActivitySplash.this, ActivityLogin.class);
-                    //Removing HomeActivity from the back stack
-                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                    startActivity(intent);
-                }
+                //Removing HomeActivity from the back stack
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                startActivity(intent);
+                //overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
             }
-        }, SPLASH_TIME_OUT);
 
-
+        } else {
+            Intent intent = new Intent(ActivitySplash.this, ActivityLogin.class);
+            //Removing HomeActivity from the back stack
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+            startActivity(intent);
+        }
     }
 }
 
